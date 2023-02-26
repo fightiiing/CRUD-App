0dbf330bc26e7ddb54698ed6e5adf9d043157b6e
Mike Giordano <pitiiing@gmail.com>
2023-02-26 15:21:37 -0500

    basic setup of todo list

---
+++ src/components/CustomForm.js
@@ -0,0 +1,39 @@
+
+const CustomForm = () => {
+  
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
+    return (
+    <form className="todo"
+    onSubmit={handleFormSubmit}
+    >
+    <div className="wrapper">
+       <input 
+       type="text"
+       id="task"
+       className="input"
+       //value={task}
+       //onInput={(e) => setTask(e.target.value)}
+       required
+       autoFocus
+       maxLength={60}
+       placeholder="Enter Task"
+        />
+       <label 
+       htmlFor="task"
+       className="label">
+        Enter Task
+        </label>
+    </div>
+<button 
+className="btn"
+aria-label="Add task"
+type="submit"
+>
+    </button> 
+    </form>
+  )
+}
+export default CustomForm
