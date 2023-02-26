33709b94471de08d9107d3c8f3c33ec7e4d30e84
Mike Giordano <pitiiing@gmail.com>
2023-02-26 17:50:07 -0500

    toggle tasks

--- src/App.js
+++ src/App.js
@@ -6,19 +6,36 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const addTask = (task) => {
-    setTasks(prevState => [
-      ...prevState, task])
+    setTasks(prevState => [...prevState, task])
+  }
+
+  const deleteTask = (id) => {
+    setTasks(prevState => prevState.filter(t => t.id !== id));
+  }
+
+  const toggleTask = (id) => {
+    setTasks(prevState => prevState.map(t=> (
+      t.id === id
+      ? {...t, checked: !t.checked}
+      : t
+    )))
   }
 
   return (
     <div className="container">
       <header>
         <h1>My Task List</h1>
-      </header> 
+      </header>
       <CustomForm addTask={addTask}/>
-    {tasks && <TaskList tasks={tasks}/>}
+      {tasks && (
+        <TaskList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+        />
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,13 +1,15 @@
 import TaskItem from './TaskItem';
 import styles from './TaskList.module.css';
 
-const TaskList = ({tasks}) => {
+const TaskList = ({tasks, deleteTask, toggleTask}) => {
   return (
     <ul className={styles.tasks}>
        {tasks.sort((a, b) => b.id - a.id).map(task=> (
         <TaskItem
             key={task.id}
             task={task}
+            deleteTask={deleteTask}
+            toggleTask={toggleTask}
         />
        ))
        }
